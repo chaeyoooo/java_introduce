b5fb2921164f2c749cab40b89c342df601b0051b
Chaeyeoncho <gcy9293@naver.com>
2025-03-23 03:26:30 +0900

    변수 초기화

---
+++ src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a);
+// 선언 하고 초기화 안하면 컴파일 x , 다른 값이 들어와서 오류 나니까 초기화를 강제함
+
+    }
+}
