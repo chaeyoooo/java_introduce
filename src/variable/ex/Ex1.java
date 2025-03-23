0619cb5bffd036a09338d899f70355796370135f
Chaeyeoncho <gcy9293@naver.com>
2025-03-23 16:01:57 +0900

    변수 문제 풀이

---
+++ src/variable/ex/Ex1.java
@@ -0,0 +1,14 @@
+package variable.ex;
+
+public class Ex1 {
+    // 4,3을 다른 숫자로 한번에 변경할 수 있도록 다음을 변수 num1 , numb2를 사용하도록 변경해보세요
+    public static void main(String[] args) {
+         int num1 = 4;
+         int num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+
+    }
+}
---
+++ src/variable/ex/Ex3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class Ex3 {
+    // long 타입의 변수를 선언하고 그 변수를 백억으로 초기화 한 후 출력하는 프로그램을 작성하세요
+    // boolean 타입의 변수를 선언하고 , 그 변수를 true로 초기화 한 후 출력하는 프로그램을 작성하세요
+    public static void main(String[] args) {
+        long longvar = 100000000L;
+        boolean booleanVar = true;
+        System.out.println(longvar);
+        System.out.println(booleanVar);
+    }
+}
