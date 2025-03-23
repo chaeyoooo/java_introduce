3db8607f0b4f05cdc5e82ed78864285898b97acb
Chaeyeoncho <gcy9293@naver.com>
2025-03-23 15:36:36 +0900

    변수 타입

---
+++ src/Var7.java
@@ -0,0 +1,15 @@
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; // a는 변수 100 은 literal , 변수의 값은 바뀔 수 있지만 리터럴은 안바뀜 , 100을 200으로 바꾸는것은 코드를 바꾸는 것이지 리터럴을 바꾸는 것은 아님
+        double b = 10.5;
+        boolean c = true; // 불리언 true , false 입력가능
+        char d = 'A'; // 문자 하나
+        String e = "Hello java"; // 문자열 , 문자열을 다루기위한 특별한 타입
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
