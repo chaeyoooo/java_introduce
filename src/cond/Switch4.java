818d522ce3cf76a5933d5a7a8371ba4e0aacf7e1
Chaeyeoncho <gcy9293@naver.com>
2025-03-24 18:07:05 +0900

    switch문

---
+++ src/cond/Switch4.java
@@ -0,0 +1,14 @@
+//package cond;
+//
+//public class Switch4 {
+//    public static void main(String[] args) {
+//        int grade = 2;
+//
+//        int coupon = switch(grade){
+//            case 1 -> 1000;
+//            case 2 -> 2000;
+//        };
+//        System.out.println(coupon);
+//    }
+//}
+// 새로운 switch 문 근데 자바 21 부터 적용 됨 ..!
