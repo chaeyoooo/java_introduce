b90463b237642304e601314b16e4b0f9aa780068
Chaeyeoncho <gcy9293@naver.com>
2025-03-25 22:34:34 +0900

    for문

---
+++ src/loop/For1.java
@@ -0,0 +1,10 @@
+package loop;
+
+public class For1 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            // command + z : 자동완성 시켜줌
+            System.out.println(i);
+        }
+    }
+}
