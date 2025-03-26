c2db28db96b22b02849b8012a4e24be79fe54675
Chaeyeoncho <gcy9293@naver.com>
2025-03-26 23:31:33 +0900

    유지보수 , 메모리 관점에서 효율적 : for문

--- src/loop/While2_2.java
+++ src/loop/While2_2.java
@@ -10,6 +10,7 @@ public class While2_2 {
             sum = sum + i;
             System.out.println("i= " + i + " sum= " + sum );
             i++;
+            //여기서 i는 main에 있으니까 코드 범위가 넓어지는 단점 for은 for문 안에서만 i에서만 쓰니까! 유지보수,메모리 관점에서 더 좋음
 
         }
     }
