2489e5d1e86aea22395d37b898f97cbd2de2c810
Chaeyeoncho <gcy9293@naver.com>
2025-03-26 20:08:54 +0900

    while문

---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,13 @@
+package loop.ex;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int count = 1; // 갯수
+        int num = 2; // 숫자
+        while(count <=10){
+            System.out.println(num);
+            num +=2;
+            count ++;
+        }
+    }
+}
