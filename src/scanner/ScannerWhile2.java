e50121a50c95a726a1a4481bd44c390c6d1c1327
Chaeyeoncho <gcy9293@naver.com>
2025-03-27 13:43:42 +0900

    scanner while

---
+++ src/scanner/ScannerWhile2.java
@@ -0,0 +1,25 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhile2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("첫번째와 두번쨰 숫자 모두 0을 입력하면 프로그램을 종료 합니다.");
+        while(true){
+            System.out.print("첫번쨰 숫자를 입력하세요 : ");
+            int num1 = scanner.nextInt();
+            System.out.print("번쨰 숫자를 입력하세요 : ");
+            int num2 = scanner.nextInt();
+
+            if(num1 == 0 && num2 == 0){
+                System.out.println("프로그램 종료");
+                break;
+            }
+
+            int sum = num1 + num2;
+            System.out.println("두 숫자의 합 :" + sum);
+        }
+    }
+}
