a90cf5361e38e696f2075e2fe5721cb8d7df2ed0
Chaeyeoncho <gcy9293@naver.com>
2025-03-28 12:22:57 +0900

    one dimension array refactoring

---
+++ src/array/Array1.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int student1 = 90;
+        int student2 = 90;
+        int student3 = 90;
+        int student4 = 90;
+        int student5 = 90;
+
+        System.out.println("학생 1 점수 " + student1);
+        System.out.println("학생 2 점수 " + student2);
+        System.out.println("학생 3 점수 " + student3);
+        System.out.println("학생 4 점수 " + student4);
+        System.out.println("학생 5 점수 " + student5);
+    }
+}
